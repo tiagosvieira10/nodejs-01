7d8aa7873f7901ccc634df84bfdb07143ea04b77
tiagosvieira10 <tiagosvieira10@gmail.com>
2024-03-07 08:23:13 -0300

    json add

--- src/server.js
+++ src/server.js
@@ -6,7 +6,9 @@ const server = http.createServer((req, res) => {
   const { method, url } = req
 
   if (method === 'GET' && url === '/users') {
-    return res.end('Listagem de usuarios')
+    return res
+    .setHeader('Content-type', 'application/json')
+    .end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
