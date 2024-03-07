d0daf539fdbd618c3575c8ffc604a90ba622e6f7
tiagosvieira10 <tiagosvieira10@gmail.com>
2024-03-07 12:55:03 -0300

    middelware

--- src/server.js
+++ src/server.js
@@ -1,27 +1,31 @@
 import http  from 'node:http'
+import { json } from './middleware/json.js'
 
 const users = []
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req
+  
+  await json(req, res)
 
   if (method === 'GET' && url === '/users') {
     return res
-    .setHeader('Content-type', 'application/json')
-    .end(JSON.stringify(users))
+      .end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
+    const { name, email } = req.body
+
     users.push({
       id: 1,
-      name: 'John Doe',
-      email: 'jhondoe@example.com'
+      name,
+      email,
     })
 
-    return res.end('Criação de usuários')
+    return res.writeHead(201).end()
   }
   
-  return res.end('Hello world')
+  return res.writeHead(404).end()
 })
 
 server.listen(3333)
