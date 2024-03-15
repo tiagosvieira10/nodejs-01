aacdbe3af046906ef995ff7f5cf66c550a2a1cc0
tiagosvieira10 <tiagosvieira10@gmail.com>
2024-03-15 15:08:33 -0300

    randomUUID e chamadas a db.json

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 import http  from 'node:http'
+import { randomUUID } from "node:crypto"
 import { json } from './middleware/json.js'
+import { Database } from './middleware/database.js'
 
-const users = []
+// UUID = Universal Unique ID
+
+const database = new Database()
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
@@ -9,18 +13,21 @@ const server = http.createServer(async (req, res) => {
   await json(req, res)
 
   if (method === 'GET' && url === '/users') {
-    return res
-      .end(JSON.stringify(users))
+    const users = database.select('users')
+    
+    return res.end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
     const { name, email } = req.body
 
-    users.push({
-      id: 1,
+    const user= {
+      id: randomUUID(),
       name,
       email,
-    })
+    }
+
+    database.insert('users',user)
 
     return res.writeHead(201).end()
   }
