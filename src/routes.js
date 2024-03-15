a39f553af4b2c9d23c59e39b8fd308f379d00ab5
tiagosvieira10 <tiagosvieira10@gmail.com>
2024-03-15 16:11:11 -0300

    regex adicionado

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import { Database } from "./database.js"
 import { randomUUID } from "node:crypto"
+import { buildRoutePath } from "./utils/build-route-path.js"
 
 // UUID = Universal Unique ID
 
@@ -8,7 +9,7 @@ const database = new Database()
 export const routes = [
   {
     method: 'GET',
-    path: '/users',
+    path:buildRoutePath('/users'),
     handler: (req, res) => {
       const users = database.select('users')
     
@@ -17,7 +18,7 @@ export const routes = [
   },
   {
     method: 'POST',
-    path: '/users',
+    path:buildRoutePath('/users'),
     handler: (req, res) => {
       const { name, email } = req.body
 
@@ -31,5 +32,13 @@ export const routes = [
   
       return res.writeHead(201).end()
     }
+  },
+  {
+    method: 'DELETE',
+    path:buildRoutePath('/users/:id'),
+    handler: (req, res) => {
+
+    }
   }
+
 ]
