2070b67bd5bf529510bcbfc600a35f3909f925ef
tiagosvieira10 <tiagosvieira10@gmail.com>
2024-03-19 12:34:47 -0300

    regex modificado

--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -1,6 +1,9 @@
 // /users/:id
 export function buildRoutePath(path) {
   const routeParametersRegex = /:([a-zA-Z]+)/g
+  const pathWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-z0-9\-_]+)')
 
-  console.log(Array.from(path.matchAll(routeParametersRegex)))
+  const pathRegex = new RegExp(`ˆ${pathWithParams}`) 
+
+  return pathRegex
 }
